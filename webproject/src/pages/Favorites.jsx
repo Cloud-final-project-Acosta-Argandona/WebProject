e7e8c3fb358fd27f1f4acf45dc32eca9addf381d
Desire_Acosta <desiree.acosta.60086@usalesiana.edu.bo>
2024-09-02 03:29:58 -0400

    Add styles for favorites

--- webproject/src/pages/Favorites.jsx
+++ webproject/src/pages/Favorites.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { fetchUsersWithFavoriteSongs } from '../repositories/UserRepository';
-import { ListGroup, Card, Col, Row, Tab, Tabs, Accordion } from 'react-bootstrap';
+import { Card, Col, Row, Accordion, Container } from 'react-bootstrap';
 import AppBar from '../components/AppBar';
+import "../styles/Favorites.css"
+import { MusicNoteBeamed, StarFill } from 'react-bootstrap-icons';
 
 const Favorites = () => {
   const [usersWithFavorites, setUsersWithFavorites] = useState([]);
@@ -17,36 +19,40 @@ const Favorites = () => {
 
   return (
     <>
-      <AppBar />
-      <h1>User's favorites</h1>
-      <Row className="mt-4">
-        <Col md={12}>
-          <Accordion>
-            {usersWithFavorites.map((user, index) => (
-              <Card key={user.email}>
-                <Accordion.Item eventKey={index.toString()}>
-                  <Accordion.Header>
-                    {user.username} ({user.email})
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    {user.favoriteSongs.length > 0 ? (
-                      <ul>
-                        {user.favoriteSongs.map((song) => (
-                          <li key={song.id}>
-                            {song.name} - {song.artistName}
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>No favorites</p>
-                    )}
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Card>
-            ))}
-          </Accordion>
-        </Col>
-      </Row>
+      <Container className='user-container user-list'>
+        <AppBar />
+        <h1>User's favorites</h1>
+        <Row className="mt-4">
+          <Col md={12}>
+            <Accordion>
+              {usersWithFavorites.map((user, index) => (
+                <Card key={user.email}>
+                  <Accordion.Item className='custom-accordion' eventKey={index.toString()}>
+                    <Accordion.Header>
+                      <StarFill className='mx-3' size={24}/>
+                      {user.username} ({user.email})
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      {user.favoriteSongs.length > 0 ? (
+                        <ul className='favorites-list'>
+                          {user.favoriteSongs.map((song) => (
+                            <li key={song.id}>
+                              <MusicNoteBeamed className="mx-3" size={24}/>
+                              {song.name} - {song.artistName}
+                            </li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <p>No favorites</p>
+                      )}
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Card>
+              ))}
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
